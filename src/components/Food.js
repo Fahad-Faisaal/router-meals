943f80c6c1f96220578209ed9de35b3fa1c1406c
Fahad-Faisaal <fahads1ab@gmail.com>
2022-03-30 19:17:20 +0600

    styles updated at food

--- src/components/Food.js
+++ src/components/Food.js
@@ -4,11 +4,12 @@ const Food = ({food}) => {
   const {strMealThumb, strMeal, strCategory} = food
   console.log(food);
   return (
-    <div className='w-64 rounded-lg shadow-lg'>
-      <img className='w-64 mb-1 rounded-t-lg' src={strMealThumb} alt="" />
-      <div className='pl-1'>
+    <div className='w-64 h-96 rounded-lg shadow-lg'>
+      <img className='w-64 h-60 mb-1 rounded-t-lg' src={strMealThumb} alt="" />
+      <div className='h-36 pl-1 pb-3 flex flex-col justify-around'>
         <h1 className='font-bold text-xl text-gray-800'>{strMeal}</h1>
         <p className='font-medium text-lg'>Category: {strCategory}</p>
+        <button className='bg-green-600 text-gray-900 py-2 px-4 font-bold w-1/2 rounded-lg hover:bg-green-500 duration-200'>See Details</button>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-9'>
       {
         foods.map(food => <Food 
           key={food.idMeal} 
